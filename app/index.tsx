024e2a7dc08abb227d34f4d059d55a916dc49cd5
Selena Siri <selenasiri53@gmail.com>
2025-10-06 17:19:52 -0400

    tailwind set primary colors

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,9 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <View className="flex-1 justify-center items-center"
     >
-      <Text>Welcome Movie App2</Text>
+      <Text className="text-5xl text-primary font-bold">Welcome Movie App2</Text>
     </View>
   );
 }
