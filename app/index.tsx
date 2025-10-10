eb477bc92b19b2b7f79e4bc687e9cb338ea319c0
Selena Siri <selenasiri53@gmail.com>
2025-10-09 23:19:14 -0400

    layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,12 @@
 import { Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center"
     >
-      <Text className="text-5xl text-primary font-bold">Welcome Movie App2</Text>
+      <Text className="text-5xl text-accent">Welcome Movie App2</Text>
+        <Link href="/onboarding">Onboarding</Link>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
     extend: {
         colors: {
             primary: '#030014',
+            secondary: '#151312',
+            light: {
+                100: '#D6C6FF',
+                200: '#A8B5DB',
+            },
+            dark: {
+                100: '#221f3D',
+                200: '#0f0d23',
+            },
+            accent: '#AB8BFF',
         }
     },
   },
